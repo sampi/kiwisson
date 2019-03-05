4a17743e398d8cb9211e5d1dd734a2ef1bcc1c63
Daniel Spitzer <dsp@tradeshift.com>
2019-03-05 01:56:35 +0100

    Updates

---
+++ precache-manifest.62ad919c66c257f5bb13d08c6d40dc9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bef395edd3f05afadfea",
+    "url": "/static/js/words.bef395ed.chunk.js"
+  },
+  {
+    "revision": "387ccc1b8d2aeecbb79e",
+    "url": "/static/js/server.387ccc1b.chunk.js"
+  },
+  {
+    "revision": "3bbc9c623ae06ae21f53",
+    "url": "/static/js/runtime~main.3bbc9c62.js"
+  },
+  {
+    "revision": "25285405cd2b295aaa59",
+    "url": "/static/js/main.25285405.chunk.js"
+  },
+  {
+    "revision": "0996eb54a106194f2779",
+    "url": "/static/js/4.0996eb54.chunk.js"
+  },
+  {
+    "revision": "25285405cd2b295aaa59",
+    "url": "/static/css/main.e4d21384.chunk.css"
+  },
+  {
+    "revision": "54cebd837b5a951d1a10305164f9aac1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/t9-keyboard/precache-manifest.7098644b48d0cde0a44a30ec4d7edceb.js"
+  "/precache-manifest.62ad919c66c257f5bb13d08c6d40dc9f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/t9-keyboard/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
