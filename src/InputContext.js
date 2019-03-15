de2d7aeec39ff2f035f0ddad68a5ef53d5aac998
Daniel Spitzer <dsp@tradeshift.com>
2019-03-15 11:20:47 +0100

    docs(inputcontext): added comments about tuple

--- src/InputContext.js
+++ src/InputContext.js
@@ -4,5 +4,9 @@ import React from 'react';
  * InputContext
  * The current raw input of numbers and *'s
  * is stored and modified in this Context.
+ *
+ * Tuple format:
+ * [0] - input
+ * [1] - setInput
  */
 export default React.createContext(['', () => {}]);
